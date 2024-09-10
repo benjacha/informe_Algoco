ed19b85acdbb481c1741bf736a7a533ab6d57235
benjacha <116391084+benjacha@users.noreply.github.com>
2024-09-09 22:05:59 -0300

    Update quick.cpp

--- ordenamiento/codigos/quick.cpp
+++ ordenamiento/codigos/quick.cpp
@@ -5,7 +5,9 @@
 #include <chrono> 
 #include <ctime> 
 
-int partition(std::vector<int>& arr, int izq, int der) {
+// Esta función elige un elemento al azar como pivote y reordena los elementos
+// de tal forma que todos los menores al pivote queden a su izquierda y los mayores a su derecha
+int particion(std::vector<int>& arr, int izq, int der) {
     int pi = izq + rand() % (der - izq + 1);
     int pivot = arr[pi];
     std::swap(arr[der], arr[pi]);
@@ -27,47 +29,42 @@ int partition(std::vector<int>& arr, int izq, int der) {
     return izq+ent;
 }
 
-
+// Implementa el algoritmo QuickSort de manera recursiva. 
+// Divide el vector en dos subvectores utilizando el pivote y luego llama a sí misma para ordenar esas dos subpartes.
 void quickSort(std::vector<int>& arr, int izq, int der) {
     if (izq < der) {
-        int pi = partition(arr, izq, der);
-
+        int pi = particion(arr, izq, der);
         quickSort(arr, izq, pi - 1);  
         quickSort(arr, pi + 1, der); 
     }
 }
 
-
-int main() {
-    std::ifstream archivo("numeros.txt");  
-
-    if (!archivo) {
-        std::cerr << "No se pudo abrir el archivo." << std::endl;
-        return 1; 
-    }
-
+//lee los archivos de prueba recibiendo el nombre de uno abriendolo y mostrando en pantalla el tiempo de ejecución
+void procesarArchivo(const std::string& archivo) {
+    std::ifstream archivo(archivo);  
     std::vector<int> arr;  
-    int num;
 
+    int num;
     while (archivo >> num) {
         arr.push_back(num);
     }
+    
     archivo.close(); 
-    srand(time(0));
+
     auto start = std::chrono::high_resolution_clock::now();
     quickSort(arr, 0, arr.size() - 1);
     auto end = std::chrono::high_resolution_clock::now();
-
-    // std::cout << "Array ordenado: ";
-    
-    // for (int i : arr) {
-    //     std::cout << i << " ";
-    // }
-
-    std::cout << std::endl;
-    
     std::chrono::duration<double, std::milli> duration = end - start;
-    std::cout << "El algoritmo se demoró " << duration.count() << " ms." << std::endl;
 
+    std::cout << "El algoritmo se demoró " << duration.count() << " ms en ordenar " << archivo << "." << std::endl;
+}
+
+int main() {
+    // Procesar los 4 archivos caso1.txt, caso2.txt, caso3.txt y caso4.txt
+    procesarArchivo("caso1.txt");
+    procesarArchivo("caso2.txt");
+    procesarArchivo("caso3.txt");
+    procesarArchivo("caso4.txt");
+    procesarArchivo("numeros.txt");
     return 0;
 }
