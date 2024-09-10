fb1ec7e907973a90b369c40f65d5bea16a0d983e
benjacha <116391084+benjacha@users.noreply.github.com>
2024-09-09 21:49:12 -0300

    Update sorting.cpp

--- ordenamiento/codigos/sorting.cpp
+++ ordenamiento/codigos/sorting.cpp
@@ -1,44 +1,39 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>  // Necesario para funcion sort
+#include <algorithm>
 #include <fstream>
 #include <chrono> 
-#include <ctime> 
 
+//utiliza el sort implementado en la biblioteca <algorithm>
 void sorting(std::vector<int>& arr){
     std::sort(arr.begin(), arr.end());
-    return;
 }
 
-int main() {
-    std::ifstream archivo("numeros.txt");  // Abrir el archivo en modo lectura
-    
-    if (!archivo) {
-        std::cerr << "No se pudo abrir el archivo." << std::endl;
-        return 1;  // Salir si no se puede abrir el archivo
-    }
-    std::vector<int> arr;  // Vector para almacenar los números leídos
-    
+//lee los archivos de prueba recibiendo el nombre de uno abriendolo y mostrando en pantalla el tiempo de ejecución
+void procesarArchivo(const std::string& archivo) {
+    std::ifstream archivo(archivo);  
+    std::vector<int> arr;  
+
     int num;
     while (archivo >> num) {
         arr.push_back(num);
     }
     archivo.close(); 
-    
+
     auto start = std::chrono::high_resolution_clock::now();
     sorting(arr);
     auto end = std::chrono::high_resolution_clock::now();
-    
-    // std::cout << "Array ordenado: ";
-    
-    // for (int i : arr) {
-    //     std::cout << i << " ";
-    // }
-
-    std::cout << std::endl;
-    
     std::chrono::duration<double, std::milli> duration = end - start;
-    std::cout << "El algoritmo se demoró " << duration.count() << " ms." << std::endl;
 
+    std::cout << "El algoritmo se demoró " << duration.count() << " ms en ordenar " << archivo << "." << std::endl;
+}
+
+int main() {
+    // Procesar los 4 archivos caso1.txt, caso2.txt, caso3.txt y caso4.txt
+    procesarArchivo("caso1.txt");
+    procesarArchivo("caso2.txt");
+    procesarArchivo("caso3.txt");
+    procesarArchivo("caso4.txt");
+    procesarArchivo("numeros.txt");
     return 0;
 }
